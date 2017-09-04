3574ef2cc2e1b41158fd2273cc806344c5897a76
artint-liu <artint.liu@gmail.com>
2017-09-05 04:41:50 +0800

    +增加了一个计时器调度的类。功能还没有完成

---
+++ clstd/clSchedule.cpp
@@ -0,0 +1,78 @@
+﻿#include "clstd.h"
+#include "clSchedule.h"
+
+namespace clstd
+{
+  TIMER* Schedule::Find(void* handle, u32 id)
+  {
+    HandleDict::iterator iterHandle = m_Handles.find(handle);
+    if(iterHandle == m_Handles.end()) {
+      return NULL;
+    }
+
+    IdDict::iterator iterId = iterHandle->second.find(id);
+    if(iterId == iterHandle->second.end()) {
+      return NULL;
+    }
+
+    return iterId->second;
+  }
+
+  void Schedule::CreateTimer(void* handle, u32 id, u32 elapse, TimerProc proc)
+  {
+    TIMER* t = Find(handle, id);
+    if(t) {
+      t->elapse = elapse;
+      t->proc = proc;
+
+      CountDownOrder::iterator iter_order = m_Order.find(t->count_down);
+      ASSERT(iter_order != m_Order.end()); // 肯定有啊！
+
+      // 从队列中移除timer，移除后队列对应位置可能是个NULL
+      TIMER** ppCurr = &iter_order->second;
+      do {
+        if(*ppCurr == t)
+        {
+          *ppCurr = t->pNext; // t->pNext 有可能是NULL
+          t->pNext = NULL;
+          break;
+        }
+        ppCurr = &((*ppCurr)->pNext);
+      } while(*ppCurr);
+      
+      t->count_down = 0;
+    }
+    else {
+      t = new TIMER(handle, id, elapse, proc);
+      HandleDict::iterator iterHandle = m_Handles.find(handle);
+      if(iterHandle == m_Handles.end())
+      {
+        IdDict dict;
+        iterHandle = m_Handles.insert(clmake_pair(handle, dict)).first;
+      }
+
+      iterHandle->second.insert(clmake_pair(id, t));
+    }
+
+    ASSERT(t->count_down == 0);
+    ASSERT(t->pNext == NULL);
+
+    CountDownOrder::iterator iter_order = m_Order.find(0);
+    if(iter_order == m_Order.end()) {
+      m_Order.insert(clmake_pair(0, t));
+    }
+    else {
+      t->pNext = iter_order->second;
+      iter_order->second = t;
+    }
+  }
+
+  void Schedule::DestroyTimer(void* handle, u32 id)
+  {
+    TIMER* t = Find(handle, id);
+    if(t) {
+      SET_FLAG(t->flags, TIMER::TimerFlag_Useless);
+    }
+  }
+
+} // namespace clstd
---
+++ clstd/clSchedule.h
@@ -0,0 +1,58 @@
+﻿#ifndef _CLSTD_TIMER_SCHEDULE_H_
+#define _CLSTD_TIMER_SCHEDULE_H_
+
+namespace clstd
+{
+  typedef void (CL_CALLBACK *TimerProc)(void* handle, u32 id);
+
+  struct TIMER
+  {
+    enum TimerFlag
+    {
+      TimerFlag_Useless = 0x0001,   // 销毁
+    };
+    TIMER* pNext;
+    void* handle;
+    TimerProc proc;
+    u32   flags;
+    u32   id;
+    u32   elapse;
+    u32   count_down;
+
+    TIMER(void* _handle, u32 _id, u32 _elapse, TimerProc _proc)
+      : pNext(NULL)
+      , handle(_handle)
+      , proc(_proc)
+      , flags(0)
+      , id(_id)
+      , elapse(_elapse)
+      , count_down(0)
+    {}
+
+    //TIMER()
+    //  : handle(NULL)
+    //  , id(0)
+    //  , elapse(0)
+    //  , proc(NULL)
+    //{}
+  };
+
+  class Schedule
+  {
+  protected:
+    typedef clmap<u32, TIMER*>        IdDict;
+    typedef clhash_map<void*, IdDict> HandleDict;
+    typedef clmap<u32, TIMER*>        CountDownOrder;
+  
+    HandleDict m_Handles;
+    CountDownOrder m_Order;
+  protected:
+    TIMER* Find(void* handle, u32 id);
+
+  public:
+    void CreateTimer(void* handle, u32 id, u32 elapse, TimerProc proc);
+    void DestroyTimer(void* handle, u32 id);
+  };
+} // namespace clstd
+
+#endif // _CLSTD_TIMER_SCHEDULE_H_
